816baf940a67d1f5bb6b177e98dea301e0f8dc0a
Elizabeth <estrellitacalderon128@gmail.com>
2025-03-02 15:16:11 -0600

    Primer Commit

---
+++ Cookies_3716368/Cookies.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cookies_3716368
+{
+	public partial class Cookies : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            // Save the cookies.
+            Response.Cookies["ddlCategory"].Value = ddlCategory.SelectedValue;
+            Response.Cookies["ddlSupplier"].Value = ddlSupplier.SelectedValue;
+            Response.Cookies["strProduct"].Value = txtProduct.Text;
+            Response.Cookies["strDescription"].Value = txtDescription.Text;
+            Response.Cookies["strImage"].Value = txtImagen.Text;
+            Response.Cookies["decPrice"].Value = txtPrice.Text;
+            Response.Cookies["bytNumberInStock"].Value = txtNumberInStock.Text;
+            Response.Cookies["bytNumberOnOrder"].Value = txtNumberOnOrder.Text;
+            Response.Cookies["bytReorderLevel"].Value = txtReorderLevel.Text;
+
+            // Go to the confirmation page.
+            Response.Redirect("ProductConfirmCookies.aspx");
+        }
+    }
+    
+}
